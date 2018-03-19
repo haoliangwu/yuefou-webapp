367d9032f88d9169b15f80151759b2e21a166f37
Lyon <wuhaoliang@cloudist.cc>
2018-03-19 16:49:20 +0800

    [IMP] 增加一些快捷操作

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatButtonModule, MatMenuModule, MatToolbarModule, MatIconModule, MatCardModule, MatFormFieldModule, MatOptionModule, MatSelectModule, MatInputModule, MatCheckboxModule, MatSliderModule, MatSlideToggleModule, MatRadioModule, MatProgressSpinnerModule, MatSidenavModule, MatGridListModule, MatDividerModule, MatListModule, MatExpansionModule, MatDialogModule, MatChipsModule } from '@angular/material';
+import { MatButtonModule, MatMenuModule, MatToolbarModule, MatIconModule, MatCardModule, MatFormFieldModule, MatOptionModule, MatSelectModule, MatInputModule, MatCheckboxModule, MatSliderModule, MatSlideToggleModule, MatRadioModule, MatProgressSpinnerModule, MatSidenavModule, MatGridListModule, MatDividerModule, MatListModule, MatExpansionModule, MatDialogModule, MatChipsModule, MatTooltipModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
@@ -22,6 +22,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatDialogModule,
     MatCardModule,
     MatChipsModule,
+    MatTooltipModule,
     FlexLayoutModule
   ]
 })
