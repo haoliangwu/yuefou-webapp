0351e8e9dd9935820d4e6e7e624d2ed10026e725
Lyon <wuhaoliang@cloudist.cc>
2018-03-20 10:21:32 +0800

    [REM] 删除一些不需要的单元测试文件

--- src/app/shared/dires/enter-key.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EnterKeyDirective } from './enter-key.directive';
-
-describe('EnterKeyDirective', () => {
-  it('should create an instance', () => {
-    const directive = new EnterKeyDirective();
-    expect(directive).toBeTruthy();
-  });
-});
