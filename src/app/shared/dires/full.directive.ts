79773b964e5ef59071a00f4277b0d601bef8f96b
Lyon <wuhaoliang@cloudist.cc>
2018-03-20 11:04:19 +0800

    [IMP] full 指令可根据 width 或 height 属性动态增加样式

--- src/app/shared/dires/full.directive.ts
+++ src/app/shared/dires/full.directive.ts
@@ -1,16 +1,29 @@
-import { Directive, Host, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, Host, ElementRef, OnInit, Renderer2, Input } from '@angular/core';
 
 @Directive({
   selector: '[appFull], [full]',
 })
 export class FullDirective implements OnInit {
+  @Input() full: string;
+
   constructor(
     @Host() private el: ElementRef,
     private renderer: Renderer2
   ) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.el.nativeElement, 'width', '100%');
-    this.renderer.setStyle(this.el.nativeElement, 'height', '100%');
+
+    switch (this.full) {
+      case 'width':
+        this.renderer.setStyle(this.el.nativeElement, 'width', '100%');
+        break;
+      case 'height':
+        this.renderer.setStyle(this.el.nativeElement, 'height', '100%');
+        break;
+      default:
+        this.renderer.setStyle(this.el.nativeElement, 'width', '100%');
+        this.renderer.setStyle(this.el.nativeElement, 'height', '100%');
+        break;
+    }
   }
 }
