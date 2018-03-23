cbea68473f932e936118977e2c74f24381fd02da
Lyon <wuhaoliang@cloudist.cc>
2018-03-23 15:48:27 +0800

    [FIX] 直接在默认的 typings.d.ts 中扩展 ramda 模块会造成冲突

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,14 +1,5 @@
-import * as R from 'ramda';
-
 /* SystemJS module definition */
 declare var module: NodeModule;
 interface NodeModule {
   id: string;
-}
-
-//augment ramda
-declare module "ramda" {
-  interface Static {
-    __(): Function;
-  }
 }
