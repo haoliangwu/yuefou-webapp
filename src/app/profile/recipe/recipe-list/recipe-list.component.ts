c72b06122f511c24b603be85fec57f1d3053ab63
Lyon <wuhaoliang@cloudist.cc>
2018-04-10 14:07:41 +0800

    [IMP] 重新设计菜谱列表页面UI

--- src/app/profile/recipe/recipe-list/recipe-list.component.ts
+++ src/app/profile/recipe/recipe-list/recipe-list.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../../model';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DialogUtilService } from '../../../shared/modules/dialog/dialog.service';
+import { RecipeService } from '../services/recipe.service';
+import { TranslateService } from '@ngx-translate/core';
+import { filter, switchMap, tap } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-recipe-list',
@@ -12,14 +17,37 @@ export class RecipeListComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private dialogUtil: DialogUtilService,
+    private recipeService: RecipeService,
+    private translate: TranslateService,
+    private toastService: ToastrService
   ) { }
 
   ngOnInit() {
   }
 
   update(recipe: Recipe) {
-    this.router.navigate(['../update/:id', { id: recipe.id }], {relativeTo: this.route});
+    this.router.navigate(['../update/:id', { id: recipe.id }], { relativeTo: this.route });
   }
 
+  delete(recipe: Recipe) {
+    const dialogRef = this.dialogUtil.confirm({
+      data: {
+        title: '确定要删除该菜谱？'
+      }
+    });
+
+    dialogRef.afterClosed().pipe(
+      filter(e => !!e),
+      switchMap(() => this.recipeService.delete(recipe.id)),
+      tap(() => {
+        this.toastService.success(this.translate.instant('TOAST.SUCCESS.BASE'));
+      })
+    ).subscribe();
+  }
+
+  share() {
+
+  }
 }
